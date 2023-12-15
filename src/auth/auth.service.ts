494e6d87d30b1fc1822640d855db969ec7ebc074
MohamedAliBouzir <bzr.medali@gmail.com>
2023-12-15 17:58:08 +0100

    added duplication exception handelling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -15,24 +16,32 @@ export class AuthService {
     //generate password hash
     const hash = await argon.hash(dto.password);
     //save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    // remove hash from the return (security) using transformers
-    delete user.hash;
-    // if you want to specify the return fields try out:
-    /**
-     * select: {
-     *  id: true,
-     *  email: true,
-     *  createdAt: true,
-     * ...};
-     */
-    // return saved user
-    return user;
-    return 'I am signed up';
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      // remove hash from the return (security) using transformers
+      delete user.hash;
+      // if you want to specify the return fields try out:
+      /**
+       * select: {
+       *  id: true,
+       *  email: true,
+       *  createdAt: true,
+       * ...};
+       */
+      // return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error;
+      }
+    }
   }
 }
