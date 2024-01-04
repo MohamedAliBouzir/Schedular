18004ff0bb852da705798cc55edca9ee69b3d63c
MohamedAliBouzir <bzr.medali@gmail.com>
2024-01-04 07:31:36 +0100

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      const config = new ConfigService();
+      const allowedOrigins = [config.get('CORS_URL'), config.get('FRONT_URL')];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Rejected due to CORS'));
+      }
+    },
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['content-type'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
